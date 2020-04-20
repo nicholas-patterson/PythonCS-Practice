6c1898cb7bc99ab6bc87d1f396bf792457f08fa5
Nicholas Patterson <nicholaspatterson36@gmail.com>
2020-04-20 03:10:38 -0400

    Adds collision checks to HashTable

--- HashTable.py
+++ HashTable.py
@@ -1,9 +1,7 @@
-class HashTable:
-    def __int__(self, array_size):
+class HashTable(object):
+    def __init__(self, array_size):
         self.array_size = array_size
         self.array_bucket = [None for item in range(array_size)]
-
-    # Create Hash Method
 
     def hash_key(self, key, count_collisions=0):
         char_bytes = key.encode()
@@ -15,7 +13,7 @@ class HashTable:
 
     def assign(self, key, value):
         array_index = self.compress_key(self.hash_key(key))
-        current_array_value = self.aray_bucket[array_index]
+        current_array_value = self.array_bucket[array_index]
 
         if current_array_value is None:
             self.array_bucket[array_index] = [key, value]
@@ -24,6 +22,26 @@ class HashTable:
         if current_array_value[0] == key:
             self.array_bucket[array_index] = [key, value]
             return
+
+        # We have collisions
+
+        number_collisions = 1
+
+        while current_array_value[0] != key:
+            new_hash_code = self.hash_key(key, number_collisions)
+            new_array_index = self.compress_key(new_hash_code)
+            current_array_value = self.array_bucket[new_array_index]
+
+            if current_array_value is None:
+                self.array_bucket[new_array_index] = [key, value]
+                return
+
+            if current_array_value[0] == key:
+                self.array_bucket[new_array_index] = [key, value]
+                return
+
+            number_collisions += 1
+            print("???", number_collisions)
 
     def retrieve(self, key):
         array_index = self.compress_key(self.hash_key(key))
@@ -35,5 +53,25 @@ class HashTable:
         if possible_return_value[0] == key:
             return possible_return_value[1]
 
-# Need to understand collisions
+        retrieval_collisions = 1
+
+        while possible_return_value[0] != key:
+            new_hash_code = self.hash_key(key, retrieval_collisions)
+            new_array_index = self.compress_key(new_hash_code)
+            possible_return_value = self.array_bucket[new_array_index]
+
+            if possible_return_value is None:
+                return None
+
+            if possible_return_value[0] == key:
+                return possible_return_value[1]
+
+            retrieval_collisions += 1
+            print("???", retrieval_collisions)
+
+    def print_hash_table(self):
+        print(self.array_bucket)
+
+
+my_hash_table = HashTable(8)
 
